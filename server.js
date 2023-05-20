2a0a793688dd65fdffd8a0ad472ceb2fffe3352e
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2023-05-20 23:11:34 +0300

    add base contact

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Olena:9qrDF8S5gGY6B0y6@cluster0.q7krt80.mongodb.net/db_contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
